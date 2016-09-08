5dc642a2509575cd3c4b506f9f688b02361fcef2
Wenxi Jin <jwenxi@gmail.com>
2016-09-08 21:22:11 +0200

    Added dir_append_datetime.py
    Simple python function for renaming a directory by appending the current
    datetime into the folder name.

---
+++ dir_append_datetime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python -u
+
+# This script is used for renaming a src directory to a new dst directory
+# dst = src_datetime
+
+import os
+import sys
+
+def dir_append_datetime(src, dst):
+    
