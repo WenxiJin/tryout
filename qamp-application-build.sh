2fe4b2d67507f54c7526290b2b8ee3ee718aceb4
Wenxi Jin <wenxi.jin@biolinscientific.com>
2016-09-24 00:38:51 +0200

    Generate .clang_complete file for qamp-application
    Use my own cc_args.rb file to generate a .clang_complete file for
    company-clang for qamp-application project.
    And it works perfect.

--- qamp-application-build.sh
+++ qamp-application-build.sh
@@ -5,4 +5,11 @@ set -e
 # absolute path
 export WORKSPACE="/home/wjn/workspace"
 export PATH=$PATH:/home/wjn/eclipse/gnutools/arm-eabi/bin
-make -r -j 4 ECOSCFG=$WORKSPACE/ecos-config-qamp-ram OBJDIR=build qamp-application.bin version.ini
+
+# CC='/home/wjn/bin/cc_args.py gcc' CXX='/home/wjn/bin/cc_args.py g++' -B \
+make V=1 \
+       -r -j 4 ECOSCFG=$WORKSPACE/ecos-config-qamp-ram OBJDIR=build qamp-application.bin version.ini \
+       > compile.log
+
+# cc_args.rb - git@github.com:WenxiJin/.clang_complete.git
+ruby /home/wjn/bin/cc_args.rb compile.log
